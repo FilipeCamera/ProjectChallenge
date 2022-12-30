23fc2bdf0979ce7db7dc9ca189e2e8d8b2b4c4d4
Filipe Camera <filipecameracastro@gmail.com>
2022-12-30 15:33:01 -0300

    refactor: refatorando caso de uso de criar usuario no banco

--- src/data/use-cases/db-create-user.ts
+++ src/data/use-cases/db-create-user.ts
@@ -1,6 +1,8 @@
 import { CreateUser } from '@domain/use-cases/create-user';
 import { UserRepository } from '../repositories/user-repository';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class DatabaseCreateUser implements CreateUser {
   constructor(private readonly userRepository: UserRepository) {}
 
