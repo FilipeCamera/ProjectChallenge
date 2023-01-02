62582c118d4f17fabf6e8b586c67da9ceb533e0c
Filipe Camera <filipecameracastro@gmail.com>
2023-01-02 17:40:41 -0300

    refactor: refatodando casos de usos e add novos packages

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,16 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
 import { DatabaseCreateUser } from '@data/use-cases/db-create-user';
+import { CryptoAdapter } from '@infra/adapters/CryptoAdapter';
+import { HashPassword } from '@data/protocols/cryptograph';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [DatabaseCreateUser],
+  providers: [
+    DatabaseCreateUser,
+    { provide: HashPassword, useClass: CryptoAdapter },
+  ],
+  exports: [HashPassword],
 })
 export class HttpModule {}
