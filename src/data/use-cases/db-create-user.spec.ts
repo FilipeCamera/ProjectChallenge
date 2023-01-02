4794fbf48499dc9c3cbb3769853e189ccae39a07
Filipe Camera <filipecameracastro@gmail.com>
2023-01-02 12:46:07 -0300

    refactor: refatorando a funcao de criar usuario no banco de dados e seu teste

--- src/data/use-cases/db-create-user.spec.ts
+++ src/data/use-cases/db-create-user.spec.ts
@@ -1,11 +1,26 @@
 import { MemoryUserRepository } from '@test/repositories/in-memory-user-database';
 import { DatabaseCreateUser } from './db-create-user';
 import { CreateUser } from '@domain/use-cases/create-user';
+import { randomUUID } from 'node:crypto';
+import { EncryptHash } from '@data/protocols/cryptograph';
+
+class EncryptMock implements EncryptHash {
+  hash: string = randomUUID();
+  pass: string;
+  async token(data: string): Promise<string> {
+    throw new Error('Method not implemented.');
+    return data;
+  }
+  async password(data: string): Promise<string> {
+    this.pass = data;
+    return this.hash;
+  }
+}
 
 describe('Database create user test', () => {
   it('should be able create user in database', async () => {
     const memoryRepository = new MemoryUserRepository();
-    const db = new DatabaseCreateUser(memoryRepository);
+    const db = new DatabaseCreateUser(memoryRepository, new EncryptMock());
 
     const data: CreateUser.request = {
       email: 'email_test_field',
@@ -20,7 +35,7 @@ describe('Database create user test', () => {
   });
   it('should be verify user exist in database', async () => {
     const memoryRepository = new MemoryUserRepository();
-    const db = new DatabaseCreateUser(memoryRepository);
+    const db = new DatabaseCreateUser(memoryRepository, new EncryptMock());
 
     const data: CreateUser.request = {
       email: 'email_test_field',
@@ -40,7 +55,7 @@ describe('Database create user test', () => {
   it('should return an error when create user with nickname that already exists', async () => {
     const memoryRepository = new MemoryUserRepository();
 
-    const db = new DatabaseCreateUser(memoryRepository);
+    const db = new DatabaseCreateUser(memoryRepository, new EncryptMock());
 
     const data: CreateUser.request = {
       email: 'email_test_field',
--- src/data/use-cases/db-create-user.ts
+++ src/data/use-cases/db-create-user.ts
@@ -1,10 +1,14 @@
 import { CreateUser } from '@domain/use-cases/create-user';
 import { UserRepository } from '../repositories/user-repository';
 import { Injectable } from '@nestjs/common';
+import { EncryptHash } from '@data/protocols/cryptograph';
 
 @Injectable()
 export class DatabaseCreateUser implements CreateUser {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly encryptHash: EncryptHash
+  ) {}
 
   async create(data: CreateUser.request): Promise<CreateUser.response> {
     const { email, nickname } = data;
@@ -15,6 +19,8 @@ export class DatabaseCreateUser implements CreateUser {
     });
 
     if (!!isUserExist) throw new Error('Email or nickname already in use');
+
+    data.password = await this.encryptHash.password(data.password);
 
     const user = this.userRepository.create(data);
 
