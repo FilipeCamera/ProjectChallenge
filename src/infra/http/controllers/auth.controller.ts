60a14cbafd9c8809d454b2e70362e52d84802f20
Filipe Camera <filipecameracastro@gmail.com>
2023-01-03 11:58:39 -0300

    refactor(auth and user): refatorando os controllers

--- src/infra/http/controllers/auth.controller.ts
+++ src/infra/http/controllers/auth.controller.ts
@@ -1,14 +1,29 @@
 import { DatabaseAuthUser } from '@data/use-cases/db-auth-user';
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthUserBody } from '../dtos/auth-user-body';
+import { DatabaseVerifyUserExist } from '@data/use-cases/db-verify-user-exist';
+import { DatabaseComparePassword } from '@data/use-cases/db-compare-password';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly dbAuthUser: DatabaseAuthUser) {}
+  constructor(
+    private readonly dbAuthUser: DatabaseAuthUser,
+    private readonly dbVerifyUserExist: DatabaseVerifyUserExist,
+    private readonly dbComparePassword: DatabaseComparePassword
+  ) {}
 
   @Post('login')
   async login(@Body() body: AuthUserBody) {
-    const token = await this.dbAuthUser.login(body);
+    const { nickname, email, password } = body;
+
+    const user = await this.dbVerifyUserExist.exec({ nickname, email });
+
+    await this.dbComparePassword.compare({
+      hashPassword: user.password,
+      plainPassword: password,
+    });
+
+    const token = await this.dbAuthUser.login(user);
 
     return token;
   }
--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -2,14 +2,20 @@ import { DatabaseCreateUser } from '@data/use-cases/db-create-user';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserBody } from '../dtos/create-user-body';
 import { UserResponse } from '../dtos/response-user';
+import { DatabaseVerifyNickOrEmailInUse } from '@data/use-cases/db-verify-nick-or-email-in-use';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly dbCreateUser: DatabaseCreateUser) {}
+  constructor(
+    private readonly dbCreateUser: DatabaseCreateUser,
+    private readonly dbVerifyNickOrEmailInUse: DatabaseVerifyNickOrEmailInUse
+  ) {}
 
   @Post()
   async create(@Body() body: CreateUserBody) {
     const { nickname, email, password, cargo, linkedin, github } = body;
+
+    await this.dbVerifyNickOrEmailInUse.exec({ nickname, email });
 
     const user = await this.dbCreateUser.create({
       nickname,
