988bb765a3c18b9a234a35616e0b8be0911d25bb
Filipe Camera <filipecameracastro@gmail.com>
2023-01-03 12:01:59 -0300

    refactor: alterando o modulo http

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,16 +2,22 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
 import { DatabaseCreateUser } from '@data/use-cases/db-create-user';
-import { CryptoAdapter } from '@infra/adapters/CryptoAdapter';
-import { HashPassword } from '@data/protocols/cryptograph';
+import { AuthController } from './controllers/auth.controller';
+import { DatabaseAuthUser } from '@data/use-cases/db-auth-user';
+import { DatabaseVerifyUserExist } from '@data/use-cases/db-verify-user-exist';
+import { DatabaseVerifyNickOrEmailInUse } from '@data/use-cases/db-verify-nick-or-email-in-use';
+import { DatabaseComparePassword } from '@data/use-cases/db-compare-password';
+import { ProtocolModule } from '@infra/protocols/protocol.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [UserController],
+  imports: [DatabaseModule, ProtocolModule],
+  controllers: [UserController, AuthController],
   providers: [
     DatabaseCreateUser,
-    { provide: HashPassword, useClass: CryptoAdapter },
+    DatabaseAuthUser,
+    DatabaseVerifyUserExist,
+    DatabaseVerifyNickOrEmailInUse,
+    DatabaseComparePassword,
   ],
-  exports: [HashPassword],
 })
 export class HttpModule {}
