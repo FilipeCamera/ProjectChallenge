77d62408f5f2b11eb7c5d42828cf8fda4dfc5445
Filipe Camera <filipecameracastro@gmail.com>
2023-01-04 14:35:04 -0300

    refactor: add nova funcionalidade

--- test/repositories/in-memory-user-database.ts
+++ test/repositories/in-memory-user-database.ts
@@ -1,6 +1,8 @@
 import { UserRepository } from '@data/repositories/user-repository';
 import { User } from '@domain/entities/user';
 import { CreateUser } from '@domain/use-cases/create-user';
+import { ReadAllUser, ReadSpecificUser } from '@domain/use-cases/read-user';
+import { UpdateUser } from '@domain/use-cases/update-user';
 import { VerifyUser } from '@domain/use-cases/verify-user';
 
 export class MemoryUserRepository implements UserRepository {
@@ -14,9 +16,35 @@ export class MemoryUserRepository implements UserRepository {
     return user;
   }
 
-  async verifyNicknameOrEmail(data: VerifyUser.request): Promise<User> {
+  async verifyNicknameOrEmail(
+    data: VerifyUser.request
+  ): Promise<VerifyUser.response> {
     return this.users.find(
       (user) => user.email === data.email || user.nickname === data.nickname
     );
+  }
+
+  async findAll(): Promise<ReadAllUser.response> {
+    return this.users;
+  }
+
+  async findById(id: string): Promise<ReadSpecificUser.response> {
+    return this.users.find((user) => user.id === id);
+  }
+
+  async update(data: UpdateUser.request): Promise<User> {
+    const { id, nickname, cargo, linkedin, github } = data;
+    this.users
+      .filter((user) => user.id === id)
+      .forEach((user) => {
+        user.nickname = nickname;
+        user.cargo = cargo;
+        user.linkedIn = linkedin;
+        user.github = github;
+      });
+
+    const updated = this.users.find((user) => user.id === id);
+
+    return updated;
   }
 }
